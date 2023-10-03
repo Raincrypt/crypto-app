8534a4b80c8d462668f4ba36676e88011eb0217c
Mousom <sarkarmousom8@gmail.com>
2023-10-04 00:30:57 +0530

    Made a small Ui Change at home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-
-
-const avatarSrc = ""; //Your Image here
+import avatarSrc from "../assets/profile_pic.jpg"
 
 const Footer = () => {
   return (
