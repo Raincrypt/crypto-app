67e656484f33fe346e7a9e8c249572ba9d6185f7
Mousom <sarkarmousom8@gmail.com>
2024-02-14 17:58:42 +0530

    Updated Nav links color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
-      <Button variant={"unstyled"} color={"white"}>
+    <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"} spacing={'2rem'}>
+      <Button variant={"unstyled"} color={"white"} _hover={{ color: 'yellow' }}>
         <Link to="/">Home</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
+      <Button variant={"unstyled"} color={"white"} _hover={{ color: 'yellow' }}>
         <Link to="/exchanges">Exchanges</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
+      <Button variant={"unstyled"} color={"white"} _hover={{ color: 'yellow' }}>
         <Link to="/coins">Coins</Link>
       </Button>
     </HStack>
